03b809d90106e8dc326cb6472671f4dcbdb72aca
bottuz <bottagisiluca@gmail.com>
2026-05-12 16:32:30 +0200

    fix config.js format

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-NEXT_PUBLIC_SUPABASE_URL=https://mggpasliowgumqrmjmvt.supabase.co
-NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY=sb_publishable_rmjk4fm7fTqiF8lNwrTijg_dPpG0dae
+window.SUPABASE_URL = "https://mggpasliowgumqrmjmvt.supabase.co";
+window.SUPABASE_ANON_KEY = "sb_publishable_rmjk4fm7fTqiF8lNwrTijg_dPpG0dae";
